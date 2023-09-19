13310579386e2fde7cf7c477add40d90b85099d8
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-09-19 14:37:33 +0300

    feat: create about section

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import MainPage from '../main-page/mainPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <MainPage/>
       <Footer/>
     </div>
   );
---
+++ src/components/main-page/mainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import About from "./about";
+import './assets/css/mainPage.css';
+
+const MainPage = () => {
+  return (
+    <>
+      <About/>
+    </>
+  )
+};
+
+export default MainPage;
