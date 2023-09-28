4678802ff207fdf9668d1142e6f905e6fe9412a7
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-09-28 20:05:39 +0300

    feat: connect yandex map

--- src/components/mapPage/mapPage.tsx
+++ src/components/mapPage/mapPage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import './mapPage.css';
+import { YMaps, Map } from '@pbe/react-yandex-maps';
 
 const MapPage = () => {
 	return (
 		<div className="map-page">
 			<h1 className="map__h1 h1">Карта концертов</h1>
-			<div className="map-page__map">/////to do map yandex</div>
+			<YMaps>
+				<div className="map-page__map" id='map'>
+					<Map
+					defaultState={{ 
+						center: [55.75, 37.57],
+						 zoom: 9 
+					}}
+					width={'100%'}
+					height={'100%'}/>
+				</div>
+			</YMaps>
 		</div>
 	)
 };
