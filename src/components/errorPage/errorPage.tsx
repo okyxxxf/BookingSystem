9a68f177024007c617f1bae3c43ecead3ce42a10
Alexander Bachinskiy <okyxxxf@gmail.com>
2023-10-23 22:28:29 +0300

    refactor: refactor error page component and change top position of dropdown menu

--- src/components/errorPage/errorPage.tsx
+++ src/components/errorPage/errorPage.tsx
@@ -14,7 +14,7 @@ const ErrorPage = () => {
           </p>
         </div>
         <div className="error-message__cat">
-          <img src="https://s3-alpha-sig.figma.com/img/a3b8/1429/d85b7ae2fd3f237d533404cce2228af7?Expires=1697414400&Signature=JMRpR-1Aoe5g5ukpmEfpRaKgTZo9G9qsJWN6kn4ES~NYzwMzG8FSj-dD3KNAhICbfeUPYNpOeozm-tNvVJJtsR0f2L2-cmecMTvk-iYoq0i4FRO2aKzBW98BplIyHeGPLlsgraE3Ev4Iw-wUqGZAXhgHceHVwYoCcB2nkKW6Nhs~BgLmVjKJTbV5L3HsemEPLvFFX1QKeLzpufdHBG8WjXVnsvluM-8XxAy4P~E2v7ZTCrUUUeWEj3PMOX-CcESzArAGFsgdxSM~wkvsVMJUsD5OZIEXOg-1dwWV473ms~M5eK5RRv5QQ7lqPnuluE98oCwNoUDVx55b8kMPBPVIcg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt='cat dance'/>
+          <img src="https://static.miraheze.org/polandballruwiki/a/a2/Kittyguess.gif" alt='cat dance'/>
         </div>
       </div>
     </div>
